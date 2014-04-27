a21df6968edc68c4c599ddf95c64465af8f4c97b
Abigail Walthall <ghthor@gmail.com>
2014-04-27 18:03:22 -0400

    Basic cmd pkg based executable

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ghthor/netmon/cmds"
+)
+
+const (
+	EC_OK int = iota
+	EC_NO_CMD
+	EC_CMD_ERROR
+	EC_UNKNOWN_COMMAND
+)
+
+func showUsageAndExit(exitCode int) {
+	flag.Usage()
+	os.Exit(exitCode)
+}
+
+func main() {
+	flag.Parse()
+
+	// Check that a verb exists in the arguments
+	args := flag.Args()
+	if len(args) == 0 {
+		showUsageAndExit(EC_NO_CMD)
+	}
+
+	// Retrieve the command bound to the verb
+	cmd := cmds.MatchVerb(args[0])
+	if cmd == nil {
+		showUsageAndExit(EC_UNKNOWN_COMMAND)
+	}
+
+	// Execute the command
+	err := cmd.Exec(args[1:])
+	if err != nil {
+		showUsageAndExit(EC_CMD_ERROR)
+	}
+}
